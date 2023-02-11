60c95ebfa8449b901b559fdae7bf0024bf638f2e
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-10 23:40:48 -0700

    add xs media theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,6 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '390px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [require("tailwind-scrollbar")],
 };
