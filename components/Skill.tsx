f71ca3f6616e67829b0c1e5681b2223c943877eb
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-11 00:39:51 -0700

    update spacing and style breakpoints

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,11 +19,11 @@ function Skill({ skill, directionLeft }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(skill.image).url()}
-        className="rounded-full border object-cover h-12 w-12 md:w-16 md:h-16 xl:w-20 xl:h-20 filter group-hover:grayscale grayscale-0 transition duration-300 ease-in-out"
+        className="rounded-full border object-cover xs:h-10 xs:w-10 sm:h-14 sm:w-14 md:w-16 md:h-16 lg:w-18 lg:h-18 xl:w-20 xl:h-20 filter group-hover:grayscale grayscale-0 transition duration-300 ease-in-out"
       />
-      <div className="rounded-full absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-12 w-12 md:w-16 md:h-16 xl:w-20 xl:h-20 z-0">
+      <div className="rounded-full absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white xs:h-10 xs:w-10 sm:h-14 sm:w-14 md:w-16 md:h-16 lg:w-18 lg:h-18 xl:w-20 xl:h-20 z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-2xl font-bold text-black opacity-100">
+          <p className="xs:text-xs sm:text-lg md:text-lg lg:text-xl xl:text-2xl font-bold text-black opacity-100">
             {skill.progress}%
           </p>
         </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -16,14 +16,14 @@ function Skills({ skills }: Props) {
       transition={{ duration: 1.5 }}
       className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 xs:text-lg sm:text-lg md:text-xl base:text-2xl lg:text-3xl xl:text-3xl">
         Skills
       </h3>
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 xs:text-sm sm:text-md md:text-md base:text-lg lg:text-lg xl:text-lg">
         Hover over a skill for current profieciency
       </h3>
 
-      <div className="grid grid-cols-5 gap-5 p-0 md:pt-10 xl:pt-32">
+      <div className="grid grid-cols-5 gap-5 p-0 md:pt-20 xl:pt-20">
         {/* Get first half of skills and map */}
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
