810ded088b02f71d88d6e78e3453ba30c4ef21aa
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-11 00:06:38 -0700

    update spacing and style breakpoints

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,34 +10,34 @@ type Props = {
 };
 
 function Hero({ pageInfo }: Props) {
-    const [text, count] = useTypewriter({
-        words: [
-            `Hey! I am ${pageInfo?.name}. Thanks for stopping by`,
-            'I enjoy building apps and software that helps others',
-            'This Full Stack website was built with...',
-            'React, TypeScript, NextJS, Tailwind on the Front End and...',
-            'Sanity CMS for the Back End for...',
-            'lightening fast performance and SEO optimization!',
-        ],
-        loop: true,
-        typeSpeed: 50,
-        delaySpeed: 1000,
-        deleteSpeed: 30,
-    })
-    return (
-        <div className='h-screen flex flex-col space-y-10 items-center justify-center text-center overflow-hidden'>
-            {/* <BackgroundCircles /> */}
-            <img
-                className="relative rounded h-[400px] w-[400px] md:h-[4500px] md:w-[500px] xl:h-[550px] xl:w-[800px] mx-auto object-cover"
-                src={urlFor(pageInfo?.heroImage).url()}
-                alt=""
-            />
-            <div className="z-20 relative">
+  const [text, count] = useTypewriter({
+    words: [
+      `Hey! I am ${pageInfo?.name}. Thanks for stopping by`,
+      'I enjoy building apps and software that helps others',
+      'This Full Stack website was built with...',
+      'React, TypeScript, NextJS, Tailwind on the Front End and...',
+      'Sanity CMS for the Back End for...',
+      'lightening fast performance and SEO optimization!',
+    ],
+    loop: true,
+    typeSpeed: 50,
+    delaySpeed: 1000,
+    deleteSpeed: 30,
+  })
+  return (
+    <div className='h-screen flex flex-col space-y-10 items-center justify-center text-center overflow-hidden'>
+      {/* <BackgroundCircles /> */}
+      <img
+        className="relative rounded xs:h-[250px] xs:w-[350] sm:h-[350px] sm:w-[450px] md:h-[400px] md:w-[550px] lg:h-[450px] lg:w-[650px] xl:h-[500px] xl:w-[750px] mx-auto object-cover"
+        src={urlFor(pageInfo?.heroImage).url()}
+        alt=""
+      />
+      <div className="z-20 relative">
         <h2 className="tracking-[15px] text-sm uppercase text-gray-500 pb-2">
           {pageInfo?.role}
         </h2>
         <h1 className="text-3xl lg:text-4xl font-semibold px-10 p-2">
-          <span className="mr-3">{text}</span>
+          <span className="mr-3 xs:text-lg sm:text-lg md:text-xl base:text-2xl lg:text-3xl xl:text-3xl">{text}</span>
           <Cursor cursorColor="#61DAFB" />
         </h1>
 
